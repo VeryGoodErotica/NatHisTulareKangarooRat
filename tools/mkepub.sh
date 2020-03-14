13d2c7a1f76e479552188a8f3a2acd8851e6954f
Alice Wonder <naughty@deviant.email>
2020-03-14 05:14:30 -0700

    AceReport generation

--- tools/mkepub.sh
+++ tools/mkepub.sh
@@ -23,31 +23,31 @@ mv Book.zip NaturalHistoryOfTheTulareKangarooRat.epub
 
 sh ../tools/epubcheck.sh NaturalHistoryOfTheTulareKangarooRat.epub
 
-#if hash ace 2>/dev/null; then
-#  if [ ! -f ${CWD}/AceReport/noace.tmp ]; then
-#    ace -f -s -o AceReport NaturalHistoryOfTheTulareKangarooRat.epub
-#    rm -rf ${CWD}/AceReport/data
-#    [ ! -d ${CWD}/AceReport ] && mkdir ${CWD}/AceReport
-#    mv AceReport/data ${CWD}/AceReport/
-#    mv AceReport/report.html ${CWD}/AceReport/
-#    mv AceReport/report.json ${CWD}/AceReport/
-#    echo "Accessibility report written to AceReport directory"
-#    echo `pwd`
-#  fi
-#fi
+if hash ace 2>/dev/null; then
+  if [ ! -f ${CWD}/AceReport/noace.tmp ]; then
+    ace -f -s -o AceReport NaturalHistoryOfTheTulareKangarooRat.epub
+    rm -rf ${CWD}/AceReport/data
+    [ ! -d ${CWD}/AceReport ] && mkdir ${CWD}/AceReport
+    mv AceReport/data ${CWD}/AceReport/
+    mv AceReport/report.html ${CWD}/AceReport/
+    mv AceReport/report.json ${CWD}/AceReport/
+    echo "Accessibility report written to AceReport directory"
+    echo `pwd`
+  fi
+fi
 
 
 mv NaturalHistoryOfTheTulareKangarooRat.epub ${CWD}/
 
 popd
 
-#if hash ace 2>/dev/null; then
-#  if [ -f AceReport/.gitignore ]; then
-#    if [ ! -f AceReport/noace.tmp ]; then
-#      git commit -m "update AceReport" AceReport/report.*
-#    fi
-#  fi
-#fi
+if hash ace 2>/dev/null; then
+  if [ -f AceReport/.gitignore ]; then
+    if [ ! -f AceReport/noace.tmp ]; then
+      git commit -m "update AceReport" AceReport/report.*
+    fi
+  fi
+fi
 
 rm -rf ${TMP}
 
